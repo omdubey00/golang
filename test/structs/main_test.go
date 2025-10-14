b7cd562cf7603c61c1fd3d5b5112caf2da1c5441
TheRiddler <roastingguruji541@gmail.com>
2025-10-14 05:58:21 +0000

    interface

--- test/structs/main_test.go
+++ test/structs/main_test.go
@@ -1,38 +1,41 @@
 package structs
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 
-	t.Run("Perimeter of a rectangle of given widht and height ", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := Perimeter(rectangle)
-		want := 40.0
+	rectangle := Rectangle{10.00, 10.00}
+	got := Perimeter(rectangle)
+	want := 40.00
 
-		if got != want {
-			t.Errorf("got : %.2f , want : %.2f", got, want)
-		}
-	})
+	if got != want {
+		t.Errorf("got : %.2f , want : %.2f ", got, want) // f is the format string for floating point numbers
+	}
 }
 
 func TestArea(t *testing.T) {
-	areaTests := []struct { // we have made some changes into the struct to make it more descriptive regarding the tests.
-		name    string
-		shape   Shape
-		hasArea float64
-	}{
-		{name: "Rectangle", shape: Rectangle{10.0, 10.0}, hasArea: 100.0}, // this is called a table driven test in golang
-		{name: "Circle", shape: Circle{10.0}, hasArea: 314.1592653589793}, // here we have created a table of tests and iterated through each of
-		{name: "Triangle", shape: Triangle{10.0, 10.0}, hasArea: 50.0},    // them and tested each of them , pov they are of the same type .
-		// this is making our test more descriptive so that we can understand each field by just seeing them.
+
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+
+		if got != want {
+			t.Errorf("got : %g , want : %g ", got, want)
+		}
 	}
 
-	for _, test := range areaTests {
-		t.Run(test.name, func(t *testing.T) {
-			got := test.shape.Area()
-			if got != test.hasArea {
-				t.Errorf(" for %#v got %f , want %f ", test.shape, got, test.hasArea)
-			}
-		})
-	} // refactored the test to give each test a testcase using t.Run and was able to make the error message more descriptive.
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10, 10}
+		checkArea(t, rectangle, 100.00)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
 }
+
+// so now I have written the bare minimum code to pass the test now refactoring will be done.
+// But we have created a general way of testing as there was a clear pattern of taking shapes and finding and comparing their area.
